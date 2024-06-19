6b83a65ba7d7cfd7e963985eac5307bd339b3dcd
gololobov <gololobov.serhiy@gmail.com>
2024-06-19 18:30:49 +0300

    Добавьте файлы проекта.

---
+++ P31_CSharp/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.Text;
+
+namespace P31_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "P31 C#";
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Clear();
+
+
+            Console.WriteLine("Мене звати Сергій");
+
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(a + b);
+
+            //int c = Int32.Parse(Console.ReadLine());
+
+            //int m = (int)5.3;
+
+            //double d = 2;
+
+            //DateTime date = DateTime.Now;
+            //Console.WriteLine(date.ToLongDateString());
+            //Console.WriteLine(date.ToShortDateString());
+            //Console.WriteLine(date.ToLongTimeString());
+            //Console.WriteLine(date.ToShortDateString());
+            //Console.WriteLine(date.ToString());
+
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            //if (!a)
+            //{
+
+            //}
+            //else if (a)
+            //{
+
+            //}
+
+            //int max = a > b ? a : b;
+
+            //switch (a)
+            //{
+            //    case 3:
+            //        Console.WriteLine("Hello");
+            //        break;
+            //    case 5:
+            //    case 4:
+            //    case 12:
+            //        Console.WriteLine("jkhkj");
+            //        break;
+            //    default:
+            //        break;
+            //}
+
+            //while (true)
+            //{
+
+            //}
+
+            //do
+            //{
+
+            //} while (true);
+
+
+            //for (int i = 0; i < a; i++)
+            //{
+
+            //}
+
+            //foreach (var item in args)
+            //{
+                
+            //}
+
+            Console.ReadKey();
+        }
+    }
+}
